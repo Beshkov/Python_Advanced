7005276e89436164f2f79a71e6cd28e76499f63d
Beshkov <alexander.beshkov@gmail.com>
2021-01-21 00:30:16 +0200

    Multidimensional Lists  - Exercise 2 - 100/100 with 0 problems. Problem was there was too much white space at the input, and the max sum was <0 AKA negative.

--- Multidimensional Lists/Exercises/03 - Maximum Sum.py
+++ Multidimensional Lists/Exercises/03 - Maximum Sum.py
@@ -1,48 +1,35 @@
-dict_of_range_and_sum = {}
+import sys
+row, columns = [int(x) for x in input().split()]
 
-def matrix_maker(row):
-    matrix = []
-    for r in range(row):
-        # matrix.append(list(map(int,input().split(', '))))
-        matrix.append([int(el) for el in input().split(' ')])
-    return matrix
+matrix = []
+for _ in range(row):
+    matrix.append([int(el) for el in input().split()])
+
+max_sum = -sys.maxsize
+keeping_list = []
+size = 3
 
 
-def max_sum_range(matrix, el_row, el_col):
-    # row_1 = sum(matrix[el_row][:el_col+3])
-    # row_2 = sum(matrix[el_row + 1][:el_col+3])
-    # row_3 = sum(matrix[el_row + 2][:el_col+3])
-    # summed_matrix = 0
-    # max_summed_matrix = 0
-    # summed_matrix = row_1 + row_2 + row_3
-    # if summed_matrix > max_summed_matrix:
-    #     max_summed_matrix = summed_matrix
-    # return max_summed_matrix
-
+def max_sum_range(matrix, str_row, str_col, size):
     sum_of_range = 0
     some_helping_list = []
-    row_help_list = 0
-    for row in range(el_row, el_row + 3):
-        some_helping_list.append([])
-        for col in range(el_col, el_col + 3):
+    for row in range(str_row, str_row + size):
+        elements = []
+        for col in range(str_col, str_col + size):
             sum_of_range += matrix[row][col]
-            some_helping_list[row_help_list].append(matrix[row][col])
-        row_help_list += 1
-    dict_of_range_and_sum[sum_of_range] = some_helping_list
+            elements.append(matrix[row][col])
+        some_helping_list.append(elements)
+    return sum_of_range, some_helping_list
 
-    return sum_of_range
 
-row, columns = [int(x) for x in input().split()]
-matrix = matrix_maker(row)
-max_sum = 0
-for el_row in range(row - 2):
-    for el_col in range(columns - 2):
-        current_max = max_sum_range(matrix, el_row, el_col)
-
+for el_row in range(row - size + 1):
+    for el_col in range(columns - size + 1):
+        current_max, list_of_el = max_sum_range(matrix, el_row, el_col, size)
         if max_sum < current_max:
             max_sum = current_max
+            keeping_list = [x for x in list_of_el]
 
 print(f"Sum = {max_sum}")
-for mini_list in dict_of_range_and_sum[max_sum]:
-
-    print(" ".join([str(x) for x in mini_list]))
+for mini_list in keeping_list:
+    mini_list = [str(item) for item in mini_list]
+    print(" ".join(mini_list))
