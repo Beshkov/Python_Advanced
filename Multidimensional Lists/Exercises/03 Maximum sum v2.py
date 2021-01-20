5ac6a669505c25a865077266d399a08c5643a01c
Beshkov <alexander.beshkov@gmail.com>
2021-01-20 23:43:45 +0200

    Multidimensional Lists  - Exercise 2 - 60/100 with two problems. Probably index out of range.

---
+++ Multidimensional Lists/Exercises/03 Maximum sum v2.py
@@ -0,0 +1,38 @@
+def matrix_maker(row):
+    matrix = []
+    for r in range(row):
+        # matrix.append(list(map(int,input().split(', '))))
+        matrix.append([int(el) for el in input().split(' ')])
+    return matrix
+
+
+rows, columns = map(int, input().split())
+
+current_sum = 0
+max_sum = 0
+
+row = None
+column = None
+
+matrix = matrix_maker(rows)
+
+for r in range(rows - 2):
+    for c in range(columns - 2):
+        current_sum = matrix[r][c] + matrix[r][c + 1] + matrix[r][c + 2] \
+                      + matrix[r + 1][c] + matrix[r + 1][c + 1] + matrix[r + 1][c + 2] \
+                      + matrix[r + 2][c] + matrix[r + 2][c + 1] + + matrix[r + 2][c + 2]
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            row = r
+            column = c
+
+print(f"Sum = {max_sum}")
+for i in range(3):
+    for j in range(3):
+        print(matrix[row+i][column+j], end=" ")
+    print()
+
+# print(f"{matrix[row][column]} {matrix[row][column + 1]} {matrix[row][column + 2]}")
+# print(f"{matrix[row + 1][column]} {matrix[row + 1][column + 1]} {matrix[row + 1][column + 2]}")
+# print(f"{matrix[row + 2][column]} {matrix[row + 2][column + 1]} {matrix[row + 2][column + 2]}")
