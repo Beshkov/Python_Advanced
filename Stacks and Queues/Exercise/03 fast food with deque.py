df84260f970e91e8732ea7a8390d3b7f63d226c9
Beshkov <alexander.beshkov@gmail.com>
2021-01-24 21:38:55 +0200

    Stacks and Queues

---
+++ Stacks and Queues/Exercise/03 fast food with deque.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+quantity_of_the_food = int(input())
+
+queue_of_orders =deque([int(el) for el in input().split()])
+
+print(max(queue_of_orders))
+
+for order in range(len(queue_of_orders)):
+    if queue_of_orders[0] > quantity_of_the_food:
+        break
+    else:
+        quantity_of_the_food -= int(queue_of_orders.popleft())
+
+
+if not queue_of_orders:
+    print("Orders complete")
+else:
+    print(f"Orders left: {' '.join(map(str,queue_of_orders))}")
