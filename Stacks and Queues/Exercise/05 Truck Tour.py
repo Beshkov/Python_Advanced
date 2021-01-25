a6434969c92cfbb42db29a4bd4c79c81d15bd595
Beshkov <alexander.beshkov@gmail.com>
2021-01-25 11:17:26 +0200

    Stacks and Queues

--- Stacks and Queues/Exercise/05 Truck Tour.py
+++ Stacks and Queues/Exercise/05 Truck Tour.py
@@ -1,52 +1,35 @@
+from collections import deque
+
 n_line = int(input())
 
-petrol_pump = []    # 0 - 5
-petrol_list = []    # 1, 10, 3, 11, 7
-km_list = []        # 5, 3, 4, 20, 7
+stations = deque()
 
-
-for ind in range(n_line):
-    petrol, km = [int(el) for el in input().split()]
-    petrol_pump.append(ind)
-    petrol_list.append(petrol)
-    km_list.append(km)
-
-# km_max_index = km_list.index((max(km_list)))
-# petrol_max_index = petrol_list.index(max(petrol_list))
+# pumps = []    # 0 - 5
+# petrol_list = deque([])   # 1, 10, 3, 11, 7
+# km_list = deque([])      # 5, 3, 4, 20, 7
 #
-# if max(km_list) > max(petrol_list) and km_max_index == petrol_max_index:
-#     petrol_list.pop(petrol_max_index)
-#     km_list.pop(km_max_index)
 #
+# for ind in range(n_line):
+#     petrol, km = (int(el) for el in input().split())
+#     pumps.append(ind)
+#     petrol_list.append(petrol)
+#     km_list.append(km)
 
-discarded_km = []
-discarded_petrol = []
+for _ in range(n_line):
+    stations.append(tuple(int(x) for x in input().split()))
 
-start_at = None
+for big_circle in range(n_line):
+    is_valid = True
+    tank = 0
+    for small_circle in range(n_line):
+        tank += (stations[small_circle][0] - stations[small_circle][1])
 
-flag = False
-
-while True:
-    for i in range(len(petrol_pump), 0, -1):
-        if petrol_list[-1] >= km_list[-1]:
-            start_at = petrol_list.index(petrol_list[-1])
-            flag = True
+        if tank < 0:
+            is_valid = False
+            stations.append(stations.popleft())
             break
-        else:
-            discarded_km.append(km_list.pop())
-            discarded_petrol.append(petrol_list.pop())
 
 
-    if flag:
+    if is_valid:
+        print(big_circle)
         break
-
-print(start_at)
-
-""""
-5
-1 5
-10 3
-3 4
-10 20
-7 7
-"""
